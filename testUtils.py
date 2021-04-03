05ab333492a71085466eb06c6464dc912adced01
Anurag-Shah <anuragshah2000@gmail.com>
2021-04-03 12:00:12 +0530

    working on preprocessing

--- testUtils.py
+++ testUtils.py
@@ -1,6 +1,5 @@
 import librosa
 import numpy as np
-import torch
 from models import *
 import soundfile
 import librosa
@@ -10,6 +9,16 @@ from utils import changeOutputTempo, wav2spectrum, spectrum2wav
 nightcall, sr1 = librosa.load("input_nightcall.wav")
 stairway, sr2 = librosa.load("input_stairway.wav")
 
+def changeOutputTempo(song, targetSong, sr):
+    estimatedTempo = librosa.beat.tempo(song, sr)
+    outputTempo = librosa.beat.tempo(targetSong, sr)
+    print(estimatedTempo, outputTempo)
+    ratio = estimatedTempo / outputTempo
+    #song = rb.change_tempo(song, sr, estimatedTempo, final_tempo)
+    outsong = rb.time_stretch(song, sr, ratio)
+    newtempo = librosa.beat.tempo(outsong, sr)
+    return outsong
+
 song = changeOutputTempo(nightcall, stairway, sr1)
 
-soundfile.write("song.wav", song, sr1)
+soundfile.write(song, sr1, "song.wav")
