850ea8f8cf2f94e0ec0f81993ff8be0c6c495899
Shubham Jain <jain229@purdue.edu>
2021-04-03 03:11:31 -0400

    I cant think of a commit msg

--- esc50train.py
+++ esc50train.py
@@ -16,16 +16,17 @@ if not LOADED:
 
 	labels = []
 	for i in range(0, 2000):
-	    idx_list = re.split("[0-9]\-[0-9]+\-[A-E]\-|\.wav", files[i])
-	    
-	    print(f"label is {idx_list[1]}")
-	    labels.append(int(idx_list[1]))
+		idx_list = re.split("[0-9]\-[0-9]+\-[A-Z]\-|\.wav", files[i])
+		print(f"list: {idx_list}")
+		print(f"filenmame: {files[i]}")
+		print(f"\tlabel is {idx_list[1]}")
+		labels.append(int(idx_list[1]))
 
 	audio_data = []
 	for i in range(0, 2000):
-	    audio, sr = wav2spectrum(files[i])
-	    print(f"Loaded {files[i]}")
-	    audio_data.append(audio)
+		audio, sr = wav2spectrum("../ESC-50/audio/" + files[i])
+		print(f"Loaded {files[i]}")
+		audio_data.append(audio)
 
 	print(f"type: {type(audio)}")
 
@@ -51,10 +52,10 @@ loss_fn = nn.CrossEntropyLoss()
 epochs = 60
 
 for epoch in range(epochs):
-    model.train()
-    x, y = train_data, train_labels
-    optimizer.zero_grad()
-    print(type(train_data[0]), type(train_labels[0]))
-    x = x.to(device, dtype=torch.float32)
-    y = y.to(device, dtype=torch.long)
+	model.train()
+	x, y = train_data, train_labels
+	optimizer.zero_grad()
+	print(type(train_data[0]), type(train_labels[0]))
+	x = x.to(device, dtype=torch.float32)
+	y = y.to(device, dtype=torch.long)
 
